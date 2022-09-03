d70ee0e1ecb98763c27f5fe61b6e78e2ec23fe63
belthenrique <henrique.beltrao@sptech.school>
2022-09-03 18:47:03 -0300

    mais etapas para o cadastro

---
+++ js/cadastro.js
@@ -0,0 +1,20 @@
+
+
+function trocarPagina(){
+    let etapa = 0
+    if(etapa0.style.display == ""){
+        for(let contador = 0; contador == etapa; contador++){
+            etapa0.style.display = 'none'
+            etapa1.style.display = ''
+
+        }
+    }
+
+    // if (etapa == 1) {
+    //     etapa++
+    //     etapa0.style.display = 'none'
+    //     etapa1.style.display = 'none'
+    //     etapa2.style.display = 'flex'
+    //     isClickedButton = false
+    // }
+}
