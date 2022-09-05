3cb0d5ea049a094fc055231d8858de7ef56153fa
belthenrique <henrique.beltrao@sptech.school>
2022-09-05 08:05:33 -0300

    melhora tela de login e cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,11 +1,10 @@
-
-
 function trocarPagina(){
     let etapa = 0
     if(etapa0.style.display == ""){
         for(let contador = 0; contador == etapa; contador++){
             etapa0.style.display = 'none'
             etapa1.style.display = ''
+            document.getElementById("span_etapa").innerHTML = "Etapa 2/2"
 
         }
     }
@@ -17,4 +16,16 @@ function trocarPagina(){
     //     etapa2.style.display = 'flex'
     //     isClickedButton = false
     // }
+}
+
+function voltar(){
+    if(etapa0.style.display == ""){
+        for(let contador = 1; contador == etapa; contador--){
+            etapa0.style.display = ''
+            etapa1.style.display = 'none'
+            document.getElementById("span_etapa").innerHTML = "Etapa 1/2"
+
+        }
+    }
+
 }
