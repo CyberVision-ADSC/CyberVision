8250d4944e76c38a198ff1f5d20b2e346e4c7649
Maria Eduarda <maria.esilva@sptech.school>
2022-09-23 20:42:38 -0300

    refatoração do site e arquitetura de pastas

---
+++ public/js/dashboard.js
@@ -0,0 +1,65 @@
+function openSidebar() {
+    document.getElementById("sidebar").style.marginLeft = 0
+    document.getElementById("backgroundCloseOffCanva").style.display = 'flex'
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.marginLeft = "-250px"
+    document.getElementById("backgroundCloseOffCanva").style.display = 'none'
+}
+
+function changeView(view) {
+    switch (view) {
+        case "painel":
+            document.getElementById("painel").style.display = 'flex'
+            document.getElementById("maquinas").style.display = 'none'
+            document.getElementById("acessos").style.display = 'none'
+            document.getElementById("chamados").style.display = 'none'
+            document.getElementById("configuracao").style.display = 'none'
+            break;
+        case "maquinas":
+            document.getElementById("painel").style.display = 'none'
+            document.getElementById("maquinas").style.display = 'flex'
+            document.getElementById("acessos").style.display = 'none'
+            document.getElementById("chamados").style.display = 'none'
+            document.getElementById("configuracao").style.display = 'none'
+            break;
+        case "acessos":
+            document.getElementById("painel").style.display = 'none'
+            document.getElementById("maquinas").style.display = 'none'
+            document.getElementById("acessos").style.display = 'flex'
+            document.getElementById("chamados").style.display = 'none'
+            document.getElementById("configuracao").style.display = 'none'
+            break;
+        case "chamados":
+            document.getElementById("painel").style.display = 'none'
+            document.getElementById("maquinas").style.display = 'none'
+            document.getElementById("acessos").style.display = 'none'
+            document.getElementById("chamados").style.display = 'flex'
+            document.getElementById("configuracao").style.display = 'none'
+            break;
+        case "configuracao":
+            document.getElementById("painel").style.display = 'none'
+            document.getElementById("maquinas").style.display = 'none'
+            document.getElementById("acessos").style.display = 'none'
+            document.getElementById("chamados").style.display = 'none'
+            document.getElementById("configuracao").style.display = 'flex'
+            break;
+        default:
+            document.getElementById("painel").style.display = 'flex'
+            document.getElementById("maquinas").style.display = 'none'
+            document.getElementById("acessos").style.display = 'none'
+            document.getElementById("chamados").style.display = 'none'
+            document.getElementById("configuracao").style.display = 'none'
+            break;
+    }
+
+    var largura = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (largura <= 900) {
+        closeSidebar()
+    }
+}
+
+function loggout() {
+    console.log("sair da aplicacao")
+}
