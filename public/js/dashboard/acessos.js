1655c1a64685727fde6a7a68b960329daec470e8
Maria Eduarda <maria.esilva@sptech.school>
2022-10-09 21:57:52 -0300

    alert de acesso

--- public/js/dashboard/acessos.js
+++ public/js/dashboard/acessos.js
@@ -15,9 +15,25 @@ function atualizarAcesso() {
 }
 
 function desativarAcesso() {
-    Swal.fire({
-        icon: 'warning',
-        title: 'Oops...',
-        text: 'Tem certeza? as mudanças não poderão ser desfeitas',
-      })
+  swal.fire({
+    title: 'Você tem certeza?',
+    text: "Você não será capaz de reverter isso!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Sim, quero apagar!',
+    cancelButtonText: 'Não, cancelar!',
+    confirmButtonColor: '#FF5353',
+    cancelButtonColor: '#14B424',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'OK!',
+          'O acesso foi excluido com sucesso!',
+          'success'
+        )
+      }
+    }
+  })
 }
